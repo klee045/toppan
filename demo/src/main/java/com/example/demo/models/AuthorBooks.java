1b569039127f3e647a895f0b418948da6fafaab1
Lee Kai En <leekaien98@gmail.com>
2023-03-05 16:30:44 +0800

    Add foreign key constraints to AuthorBooks

--- demo/src/main/java/com/example/demo/models/AuthorBooks.java
+++ demo/src/main/java/com/example/demo/models/AuthorBooks.java
@@ -2,27 +2,31 @@ package com.example.demo.models;
 
 import java.time.OffsetDateTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "author_books")
 public class AuthorBooks {
 
     @EmbeddedId
     private AuthorBooksKey id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @MapsId("authorId")
-    @JoinColumn(name = "author_id")
+    @JoinColumn(name = "author_id", foreignKey = @ForeignKey(name = "author_books_author_id_fkey", foreignKeyDefinition = "FOREIGN KEY (author_id) REFERENCES public.authors (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE"))
     private Author author;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @MapsId("bookId")
-    @JoinColumn(name = "book_id")
+    @JoinColumn(name = "book_id", foreignKey = @ForeignKey(name = "author_books_book_id_fkey", foreignKeyDefinition = "FOREIGN KEY (book_id) REFERENCES public.books (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE"))
     private Book book;
 
     @Column(name = "createdAt", columnDefinition = "timestamp with time zone not null")
