f668b9d0240f1ada7f4096f2e1e2132819d9ff84
Lee Kai En <leekaien98@gmail.com>
2023-03-07 14:28:25 +0800

    Add more data on initialisation, and create sequences in schema.sql

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -80,6 +80,7 @@ INSERT INTO book_rents (person_id, book_id, created_at, updated_at) VALUES
     (1, 1, NOW(), NOW()),
     (2, 1, NOW(), NOW()),
     (2, 1, NOW(), NOW()),
+    (13, 4, NOW(), NOW()),
     (3, 1, NOW(), NOW()),
     (4, 1, NOW(), NOW()),
     (5, 1, NOW(), NOW()),
