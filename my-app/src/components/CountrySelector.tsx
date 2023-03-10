88f045603ecd779fa79d1f9178a452dd46517c71
Lee Kai En <leekaien98@gmail.com>
2023-03-10 23:39:15 +0800

    Fix button country randomizer

--- my-app/src/components/CountrySelector.tsx
+++ my-app/src/components/CountrySelector.tsx
@@ -5,7 +5,9 @@ import React, {
   useState,
 } from "react";
 
-const CountrySelectorStyle: CSSProperties = {};
+const CountrySelectorStyle: CSSProperties = {
+  display: "flex",
+};
 
 const CountrySelector = (): JSX.Element => {
   const [countrySelected, setCountrySelected]: [
@@ -15,13 +17,15 @@ const CountrySelector = (): JSX.Element => {
 
   const handleClick = (): void => {
     const countries: string[] = ["SG", "US", "MY"];
-    const randomId: number = Math.floor(
-      Math.random() * (countries.length - 0 + 1) + 0
-    );
+    const randomId: number = Math.floor(Math.random() * countries.length);
     setCountrySelected(countries[randomId]);
   };
 
-  return <button onClick={handleClick}>Get Country: {countrySelected}</button>;
+  return (
+    <button style={CountrySelectorStyle} onClick={handleClick}>
+      Get Country: {countrySelected}
+    </button>
+  );
 };
 
 export default CountrySelector;
