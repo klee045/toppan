b2b5037cfd40347ae2a8ab44c868b76b69af0657
Lee Kai En <leekaien98@gmail.com>
2023-03-12 22:20:37 +0800

    Remove /book RequestMapping from BookController to fit to requirements

--- library-api/src/main/java/com/example/libraryapi/controllers/BookController.java
+++ library-api/src/main/java/com/example/libraryapi/controllers/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,7 +20,6 @@ import com.example.libraryapi.services.BookService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/book")
 public class BookController {
 
     @Autowired
