f83eba5cad4bf1ca8a95291812736332dbfcd859
Lee Kai En <leekaien98@gmail.com>
2023-03-12 09:25:28 +0800

    Add service layer to api

--- library-api/src/main/java/com/example/libraryapi/controllers/BookController.java
+++ library-api/src/main/java/com/example/libraryapi/controllers/BookController.java
@@ -1,9 +1,6 @@
 package com.example.libraryapi.controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,9 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.libraryapi.errorhandlers.ApiError;
 import com.example.libraryapi.errorhandlers.BadRequestException;
-import com.example.libraryapi.repositories.BookRepository;
-
-import jakarta.persistence.NoResultException;
+import com.example.libraryapi.errorhandlers.NoResultException;
+import com.example.libraryapi.services.BookService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -29,73 +25,14 @@ import jakarta.persistence.NoResultException;
 public class BookController {
 
     @Autowired
-    BookRepository bookRepository;
+    BookService bookService;
 
     @GetMapping("/getTop3ReadBooks")
     public ResponseEntity<List<Object>> getTop3ReadBooks(
             @RequestParam(value = "country_code", required = false) String countryCode)
             throws BadRequestException, NoResultException {
-        Long countryId = null;
-
-        // Get the corresponding countryId for the input countryCode if not null
-        if (countryCode != null) {
-            if (countryCode.toUpperCase().equals("SG")) {
-                countryId = Long.valueOf(702);
-            } else if (countryCode.toUpperCase().equals("MY")) {
-                countryId = Long.valueOf(458);
-            } else if (countryCode.toUpperCase().equals("US")) {
-                countryId = Long.valueOf(840);
-            } else {
-                throw new BadRequestException("invalid parameter");
-            }
-        } else {
-            throw new BadRequestException("invalid parameter");
-        }
-        // Call the query and add the result into the books array
-        List<Map<String, Object>> books = new ArrayList<Map<String, Object>>(
-                bookRepository.getTop3BorrowedBooksInCountryAndTop3BorrowersWithinCountry(countryId));
-
-        if (books.isEmpty()) {
-            throw new NoResultException("no results");
-        }
-
-        // Format the response
-        List<Object> result = formatTop3Response(countryCode, books);
-
+        List<Object> result = bookService.getTop3ReadBooks(countryCode);
         return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    // Helper function to format query result into required json format
-    public List<Object> formatTop3Response(String countryCode, List<Map<String, Object>> books) {
-        // use a hashmap to mimic a set through use of bookId, in the required format
-        Map<String, Object> hashMap = new HashMap<>();
-        List<Object> result = null;
-
-        for (Map<String, Object> book : books) {
-            String bookId = String.valueOf(book.get("book_id"));
-            String borrower = (String) book.get("borrower");
-
-            // if hashMap already contains this book, extend top 3 borrowers array
-            if (hashMap.containsKey(bookId)) {
-                ((List) ((HashMap) hashMap.get(bookId)).get("borrower")).add(borrower);
-                continue;
-            }
-
-            // Create a temp hash map in the required json format as the value
-            Map<String, Object> valueHashMap = new HashMap<>();
-            valueHashMap.put("author", book.get("author"));
-            valueHashMap.put("name", book.get("book"));
-            valueHashMap.put("borrower", new ArrayList<>());
-            ((List) valueHashMap.get("borrower")).add(borrower);
-
-            // Put the valueHashMap into the hashMap holder
-            hashMap.put(bookId, valueHashMap);
-        }
-
-        // Put hashMap into result
-        result = new ArrayList<>(hashMap.values());
-
-        return result;
     }
 
     /*
--- library-api/src/main/java/com/example/libraryapi/controllers/CountryController.java
+++ library-api/src/main/java/com/example/libraryapi/controllers/CountryController.java
@@ -1,9 +1,6 @@
 package com.example.libraryapi.controllers;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,8 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.libraryapi.models.Country;
-import com.example.libraryapi.repositories.CountryRepository;
+import com.example.libraryapi.services.CountryService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -22,23 +18,11 @@ import com.example.libraryapi.repositories.CountryRepository;
 public class CountryController {
 
     @Autowired
-    CountryRepository countryRepository;
+    CountryService countryService;
 
     @GetMapping("/getRandomCountry")
     public ResponseEntity<Map<String, Object>> getRandomCountry() {
-        // Get all countries in db
-        List<Country> countries = countryRepository.findAllCountries();
-
-        // Get a random number between 0 and length of countries array
-        Random rand = new Random();
-        int n = rand.nextInt(countries.size());
-
-        // Format into response requirement
-        Map<String, Object> result = new HashMap<>();
-        result.put("country", new HashMap<String, String>());
-        ((Map<String, Object>) result.get("country")).put("full_name", countries.get(n).getCountryFullName());
-        ((Map<String, Object>) result.get("country")).put("country_code", countries.get(n).getCountryCode());
-
+        Map<String, Object> result = countryService.getRandomCountry();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
