9823fbab14e135f59897361a0ded4966a6e49bd0
Lee Kai En <leekaien98@gmail.com>
2023-03-12 09:05:45 +0800

    Rename library-api layer folders to plural form

--- library-api/src/main/java/com/example/libraryapi/errorhandler/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryapi.errorhandler;
-
-public class BadRequestException extends Exception {
-    public BadRequestException(String errorMessage) {
-        super(errorMessage);
-    }
-}
--- library-api/src/main/java/com/example/libraryapi/errorhandler/NoResultException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryapi.errorhandler;
-
-public class NoResultException extends Exception {
-    public NoResultException(String errorMessage) {
-        super(errorMessage);
-    }
-}
